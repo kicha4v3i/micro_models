0f1eeabaacf9008a7b3a4f21096ffb27bc713b51
kicha4v3i <90300886+kicha4v3i@users.noreply.github.com>
2024-02-07 10:22:29 +0400

    Update WC.vue
    Added login page redirection

--- src/views/WC.vue
+++ src/views/WC.vue
@@ -2,18 +2,46 @@
 import store from '../store/index.js'
 import Sidenav from '../components/Sidenav.vue'
 import MainInputs from '../components/MainInputs.vue'
+import router from '@/router'
+
+// Redirect to login page if not logged in
+const user = store.state.user;
+
+if (!user) {
+    router.push({name: 'login'})
+}
+
+// Sidenav Collapse status
+const collapse_stat = store.sidenav_collapse
 </script>
 
 <template>
     <div class="d-flex">
-        <Sidenav />
-
-        
-        <div class="main ms-5 mx-auto mt-5 d-flex flex-column justify-content-start">
-            <h3>Well Control Formulas</h3>
+        <div>
+            <Sidenav />
+        </div>
+    
+        <div class="main ms-4 mx-auto mt-3 d-flex flex-column justify-content-start" :class="{side_collapse: collapse_stat}">
+            <h1>Micro Models</h1>
+            <hr class="w-100">
+            <h3>Well Control Formulas <span v-if="store.wcTab.activeTab">{{ '- ' + store.wcTab.tabName }}</span></h3>
             <component :is="store.wcTab.activeTab"></component>
+        </div>
+
+        <div>
             <MainInputs />
         </div>
         
     </div>
 </template>
+
+<style scoped>
+.main {
+    width: 55vw;
+    transition: ease-out 0.3s;
+}
+
+.side_collapse {
+    width: 67vw;
+}
+</style>
